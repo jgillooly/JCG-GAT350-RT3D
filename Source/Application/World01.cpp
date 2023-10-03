ec2908f1ca0cc133d1e9bf48177cbe1dab19db0d
jgillooly <122410857+jgillooly@users.noreply.github.com>
2023-10-03 11:52:25 -0600

    OpenGL setup

--- Source/Application/World01.cpp
+++ Source/Application/World01.cpp
@@ -16,22 +16,50 @@ namespace nc
 
     void World01::Update(float dt)
     {
+        m_position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_A) ? -dt : 0;
+        m_position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_D) ? dt : 0;
+        m_position.x = Clamp(m_position.x, -1.0f, 1.0f);
+        
+        m_position.y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_S) ? -dt : 0;
+        m_position.y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_W) ? dt : 0;
+        m_position.y = Clamp(m_position.y, -1.0f, 1.0f);
+
+        m_angle += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_Q) ? dt * 45 : 0;
+        m_angle += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_E) ? -dt * 45 : 0;
+
+        m_time += dt;
     }
 
     void World01::Draw(Renderer& renderer)
     {
         // pre-render
-        renderer.SetColor(0, 0, 0, 0);
         renderer.BeginFrame();
 
         // render
-        renderer.SetColor(255, 255, 255, 255);
-        for (size_t i = 0; i < 100; i++)
-        {
-            renderer.DrawPoint(random(renderer.GetWidth()), random(renderer.GetHeight()));
-            renderer.DrawLine(random(renderer.GetWidth()), random(renderer.GetHeight()), random(renderer.GetWidth()), random(renderer.GetHeight()));
-            
-        }
+
+
+
+        glPushMatrix();
+        glRotatef(m_angle, 0, 0, 1);
+        glTranslatef(m_position.x, m_position.y, 0);
+
+        //glScalef(sin(m_time), 1, 1);
+
+        glBegin(GL_TRIANGLES);
+
+        glColor3f(1, 0, 0);
+        glVertex2f(-0.5f, -0.5f);
+
+        glColor3f(0, 0, 1);
+        glVertex2f(0, 0.5f);
+
+        glColor3f(0, 1, 0);
+        glVertex2f(0.5f, -0.5f);
+
+
+        glEnd();
+
+        glPopMatrix();
 
         // post-render
         renderer.EndFrame();
--- Source/Application/World01.h
+++ Source/Application/World01.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Framework/World.h"
 #include "Core/Math/Vector2.h"
+#include <vector>
 
 namespace nc
 {
@@ -16,5 +17,7 @@ namespace nc
 		float m_angle = 0;
 		float m_time;
 		vec2 m_position;
+		std::vector<vec2> positions;
+		bool rotating = false;
 	};
 }
