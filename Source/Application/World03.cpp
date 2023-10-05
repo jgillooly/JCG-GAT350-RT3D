cfed2fe5943f3069d0a288941e9b1212ad91ccd3
jgillooly <122410857+jgillooly@users.noreply.github.com>
2023-10-05 11:52:10 -0600

    More opengl stuff :)

--- Source/Application/World03.cpp
+++ Source/Application/World03.cpp
@@ -1,4 +1,4 @@
-#include "World02.h"
+#include "World03.h"
 #include "Framework/Framework.h"
 
 #include "Input/InputSystem.h"
@@ -7,42 +7,11 @@
 
 namespace nc
 {
-    bool World02::Initialize()
+    bool World03::Initialize()
     {
         //shaders
-        const char* vertexShader =
-            "#version 430\n"
-            "layout (location=0) in vec3 position;"
-            "layout (location=1) in vec3 color;"
-            "layout (location=0) out vec3 ocolor;"
-            "void main() {"
-            "  ocolor = color;"
-            "  gl_Position = vec4(position, 1.0);"
-            "}";
-
-
-
-        const char* fragmentShader =
-            "#version 430\n"
-            "layout (location=0) in vec3 color;"
-            "out vec4 ocolor;"
-            "void main() {"
-            "  ocolor = vec4(color, 1);"
-            "}";
-
-        GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vs, 1, &vertexShader, NULL);
-        glCompileShader(vs);
-
-        GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fs, 1, &fragmentShader, NULL);
-        glCompileShader(fs);
-
-        GLuint program = glCreateProgram();
-        glAttachShader(program, vs);
-        glAttachShader(program, fs);
-        glLinkProgram(program);
-        glUseProgram(program);
+        m_program = GET_RESOURCE(Program, "Shaders/unlit_color.prog");
+        m_program->Use();
 
 #ifdef INTERLEAVE
         //vertex data
@@ -116,11 +85,11 @@ namespace nc
         return true;
     }
 
-    void World02::Shutdown()
+    void World03::Shutdown()
     {
     }
 
-    void World02::Update(float dt)
+    void World03::Update(float dt)
     {
         m_time += dt;
 
@@ -145,9 +114,12 @@ namespace nc
         if (ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_SPACE) && !ENGINE.GetSystem<InputSystem>()->GetPreviousKeyDown(SDL_SCANCODE_SPACE)) {
             automatic = !automatic;
         }
+
+        GLint uniform = glGetUniformLocation(m_program->m_program, "time");
+        glUniform1f(uniform, m_time);
     }
 
-    void World02::Draw(Renderer& renderer)
+    void World03::Draw(Renderer& renderer)
     {
         // pre-render
         renderer.BeginFrame();
